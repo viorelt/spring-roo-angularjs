8ab2cb36a472ad4b263dcecc09d9a25ae9288fc5
Viorel Tacliciu <viorelt>
2013-09-12 23:43:29 +0300

    First Commit

---
+++ src/main/java/org/viorelt/spring/roo/angluar/First.java
@@ -0,0 +1,16 @@
+package org.viorelt.spring.roo.angluar;/*
+* First.java
+*
+* Copyright (c) 2013 Teamnet. All Rights Reserved.
+*
+* This source file may not be copied, modified or redistributed,
+* in whole or in part, in any form or for any reason, without the express
+* written consent of Teamnet.
+*/
+
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
